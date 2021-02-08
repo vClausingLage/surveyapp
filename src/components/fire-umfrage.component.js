c6d3f630a7cc86240553f99e3819aa0658f99a66
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2021-02-08 07:49:16 +0100

    datatype

--- src/components/fire-umfrage.component.js
+++ src/components/fire-umfrage.component.js
@@ -1,11 +1,13 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import { set } from 'mongoose';
  
 const FireUmfrage = (  ) => {
     const [fragen, setFragen] = useState([]);
     const [optionen, setOptionen] = useState([]);
     const [UName, setUName] = useState('');
+    const [optionIndex, setOptionIndex] = useState([]);
 
     let { id } = useParams();
 
@@ -23,22 +25,30 @@ const FireUmfrage = (  ) => {
         console.log()
     }
 
+    function setInput(element, index){
+        console.log('Option: ' + element + ' Frage: ' + index);
+        setOptionIndex(optionIndex => [...optionIndex, {option: element, frage: index}]);
+    }
+
     return (
         <Fragment>
         <h1>{UName}</h1>
                 <div className="container">
                 <table>
                 <tbody>
-                <tr><th>Fragen</th>{optionen.map((item) => <th key={optionen.indexOf(item)}>{item}</th>)}</tr>
-                {fragen.map((item, frindex) => <tr key={fragen.indexOf(item) + 100}><td>{item}</td>
+                <tr><th>Fragen</th>{optionen.map((item, index) => <th key={index}>{item}</th>)}</tr>
+                
+                {fragen.map((item, frindex) => <tr key={frindex}><td>{item}</td>
                     {optionen.map((element, index) => <td key={index} className="ankreuzen">
                         <input 
                         type="radio"
                         name={frindex}
                         id={element}
+                        onChange={e => setInput(element,index)}
                         />
                     </td>)}
                 </tr>)}
+
                 </tbody>
                 </table>
             </div>
