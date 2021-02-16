ab841de4a99dedf922d2b7dc47c987557c000aa1
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2021-02-16 15:47:43 +0100

    restructure

---
+++ client/src/components/footer.component.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const Footer = () => {
+  return (
+    <Fragment>
+        <div className="panel">
+        <p>&copy; 2021 clausing-lage</p>
+        </div>
+    </Fragment>
+  );
+}
+
+export default Footer;
