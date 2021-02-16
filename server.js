a38e52a79026e05a1a12b73a58535de791bbb189
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2021-02-16 18:05:38 +0100

    server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const helmet = require('helmet');
+//const helmet = require('helmet');
 const path = require('path');
 const UmfrageRoute = require('./routes/umfrage-route');
 const PORT = process.env.PORT || 4000;
@@ -21,24 +21,24 @@ mongoose.connect(keys.mongoURI, {
 const app = express();
 
 app.use(cors());
-app.use(
-    helmet.contentSecurityPolicy({
-      directives: {
-        defaultSrc: ["'self'"],
-        connectSrc: ["'self'", 'https://surveyapp-cll.herokuapp.com'],
-        frameSrc: ["'self'", 'https://surveyapp-cll.herokuapp.com'],
-        childSrc: ["'self'", 'https://surveyapp-cll.herokuapp.com'],
-        scriptSrc: ["'self'", 'https://surveyapp-cll.herokuapp.com'],
-        styleSrc: [
-          "'self'",
-          'https://fonts.googleapis.com',
-          'https://surveyapp-cll.herokuapp.com',
-        ],
-        fontSrc: ["'self'", 'https://fonts.gstatic.com'],
-        baseUri: ["'self'"],
-      },
-    })
-  )
+//app.use(
+//    helmet.contentSecurityPolicy({
+//      directives: {
+//        defaultSrc: ["'self'"],
+//        connectSrc: ["'self'", 'https://surveyapp-cll.herokuapp.com'],
+//        frameSrc: ["'self'", 'https://surveyapp-cll.herokuapp.com'],
+//        childSrc: ["'self'", 'https://surveyapp-cll.herokuapp.com'],
+//        scriptSrc: ["'self'", 'https://surveyapp-cll.herokuapp.com'],
+//        styleSrc: [
+//          "'self'",
+//          'https://fonts.googleapis.com',
+//          'https://surveyapp-cll.herokuapp.com',
+//        ],
+//        fontSrc: ["'self'", 'https://fonts.gstatic.com'],
+//        baseUri: ["'self'"],
+//      },
+//    })
+//  )
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use('/api', UmfrageRoute);
